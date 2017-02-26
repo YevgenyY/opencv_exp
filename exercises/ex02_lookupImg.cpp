8789e0f6ed5d7b4c706f79c9b226e8ae91b5b5ad
Yevgeny V.Yorkhov <yy@ixi.ru>
2017-02-26 20:04:27 +0300

    Add manual lookup in color reduction.

--- exercises/ex02_lookupImg.cpp
+++ exercises/ex02_lookupImg.cpp
@@ -6,6 +6,32 @@
 #include <cstring>
 using namespace std;
 using namespace cv;
+
+Mat& ScanImageAndReduceC(Mat& I, const uchar* const table)
+{
+	// accept only char type matrices
+	CV_Assert(I.depth() == CV_8U);
+	int channels = I.channels();
+	int nRows = I.rows;
+	int nCols = I.cols * channels;
+	if (I.isContinuous())
+	{
+		nCols *= nRows;
+		nRows = 1;
+	}
+	int i,j;
+	uchar* p;
+	for( i = 0; i < nRows; ++i)
+	{
+		p = I.ptr<uchar>(i);
+		for ( j = 0; j < nCols; ++j)
+		{
+			p[j] = table[p[j]];
+		}
+	}
+	return I;
+}
+
 int main( int argc, char** argv ) {
 	// show help
 	if(argc<2) {
@@ -15,7 +41,7 @@ int main( int argc, char** argv ) {
 		return 0;
 	}
 	// declares all required variables
-	Mat I, J;
+	Mat I, J, H;
 	I = cv::imread( argv[1], CV_LOAD_IMAGE_COLOR);
 
 	int divideWith;
@@ -25,16 +51,18 @@ int main( int argc, char** argv ) {
 	s << argv[2];
 	s >> divideWith;
 
-	if ( !I.data || !divideWith ) 
+	if ( !I.data || !divideWith || !s )
 	{
 		cout << "The image " << argv[1] << " could not be loaded" << endl;
 		cout << "Usage bex02 image_filename divideWith" << endl;
+
+		return -1;
 	}
 
 	// create reducion table
 	cout << "Print color reduction table" << endl;
 	uchar table[256];
-	for ( int i = 0; i < 256; ++i) 
+	for ( int i = 0; i < 256; ++i)
 	{
 		table[i] = divideWith * (i/divideWith);
 		printf("%04x ", table[i]);
@@ -44,16 +72,23 @@ int main( int argc, char** argv ) {
 	Mat lookupTable(1, 256, CV_8U);
 	uchar *p = lookupTable.ptr();
 
-	for( int i = 0; i < 256; ++i ) 
+	for( int i = 0; i < 256; ++i )
 		p[i] = table[i];
 
 	// The fastest function for image color reduction
 	LUT(I, lookupTable, J);
-	cv::imshow("Ex02", J);
 
+	cv::imshow("Ex02", J);
 	cv::waitKey(0);
 
+	// Use manual lookup
+	Mat I0 = I.clone();
+	H = ScanImageAndReduceC(I0, table);
+
+	cv::imshow("Ex03", H);
+	cv::waitKey(0);
 
 	return 0;
 }
+
 
