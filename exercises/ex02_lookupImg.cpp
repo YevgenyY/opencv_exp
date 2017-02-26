ee7b4c97297b5d26c29079b8227593adf4e342ec
Yevgeny V.Yorkhov <yy@ixi.ru>
2017-02-26 19:31:07 +0300

    Add color reduction with cv::LUT function (the fastest)

--- exercises/ex02_lookupImg.cpp
+++ exercises/ex02_lookupImg.cpp
@@ -10,45 +10,49 @@ int main( int argc, char** argv ) {
 	// show help
 	if(argc<2) {
 		cout<<
-		    " Usage: ex01 <image_name>\n"
+		    " Usage: ex02 <image_name> divideWith\n"
 		    << endl;
 		return 0;
 	}
 	// declares all required variables
-	Mat A, C;
-	A = cv::imread( argv[1]);
+	Mat I, J;
+	I = cv::imread( argv[1], CV_LOAD_IMAGE_COLOR);
 
-	Mat D (A, Rect(50, 50, 2, 2) ); // using a rectangle
-	Mat E = A(Range::all(), Range(1,1000)); // using row and column boundaries
+	int divideWith;
 
-	cv::imshow("ex01", E);
-	cv::waitKey( 0 );
+	// read divideWith
+	stringstream s;
+	s << argv[2];
+	s >> divideWith;
 
-	// Mat initialization as matrix
-	// http://docs.opencv.org/3.1.0/d6/d6d/tutorial_mat_the_basic_image_container.html
-	Mat M(2,2, CV_8UC3, Scalar(0,0,255));
-	cout << "M = " << endl << " " << M << endl << endl;
+	if ( !I.data || !divideWith ) 
+	{
+		cout << "The image " << argv[1] << " could not be loaded" << endl;
+		cout << "Usage bex02 image_filename divideWith" << endl;
+	}
 
-	// Formatting output for matlab (default)
-	Mat R = Mat(3, 2, CV_8UC3);
-	randu(R, Scalar::all(0), Scalar::all(255));
-
-	cout << "R (default) = " << endl << R << endl << endl;
-	// Formatting output for python (example)
-	cout << "R (python)  = " << endl << format(R, Formatter::FMT_PYTHON) << endl << endl;
-	// Formatting output for CSV (example)
-	cout << "R (csv)     = " << endl << format(R, Formatter::FMT_CSV ) << endl << endl;
-	// Formatting output for numpy (example)
-	cout << "R (numpy)     = " << endl << format(R, Formatter::FMT_NUMPY ) << endl << endl;
+	// create reducion table
+	cout << "Print color reduction table" << endl;
+	uchar table[256];
+	for ( int i = 0; i < 256; ++i) 
+	{
+		table[i] = divideWith * (i/divideWith);
+		printf("%04x ", table[i]);
+	}
 
 
-	// output other common items
-	vector<Point2f> vPoints(20);
-	for (size_t i = 0; i < vPoints.size(); ++i)
-		vPoints[i] = Point2f((float)(i * 5), (float)(i % 7));
-	cout << "A vector of 2D Points = " << vPoints << endl << endl;
+	Mat lookupTable(1, 256, CV_8U);
+	uchar *p = lookupTable.ptr();
 
-	cout << "A = " << endl << " " << D << endl << endl;
+	for( int i = 0; i < 256; ++i ) 
+		p[i] = table[i];
+
+	// The fastest function for image color reduction
+	LUT(I, lookupTable, J);
+	cv::imshow("Ex02", J);
+
+	cv::waitKey(0);
+
 
 	return 0;
 }
